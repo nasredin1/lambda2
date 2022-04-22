f93aa57b275e02603dff62391805b9378c676179
Nikolay Shipitsyn <n_asd@mail.ru>
2022-04-23 02:31:26 +0700

    готово 02:30

---
+++ src/Worker.java
@@ -0,0 +1,17 @@
+public class Worker {
+    private OnTaskDoneListener callback;
+    private OnTaskErrorListener errorCallback;
+
+    public Worker(OnTaskDoneListener callback, OnTaskErrorListener errorCallback) {
+        this.callback = callback;
+        this.errorCallback = errorCallback;
+    }
+
+    public void start() {
+        for (int i = 0; i < 100; i++) {
+            if (i == 33) {
+                errorCallback.onError("Работа " + i + " не выполнена");
+            } else callback.onDone("Работа " + i + " выполнена");
+        }
+    }
+}
